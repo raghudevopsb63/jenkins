2de72558a9e082e73b61bbb5e367d08e10869aff
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-07 07:04:01 +0530

    Update jenkins/

--- seed-jobs/cart.groovy
+++ seed-jobs/cart.groovy
@@ -1,4 +1,4 @@
-multibranchPipelineJob('example') {
+multibranchPipelineJob('CI-Pipelines/cart') {
   branchSources {
     git {
       id('123456789') // IMPORTANT: use a constant and unique identifier
