d972fd049de84a7822278d72eba707fbc375b84b
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-07 07:31:25 +0530

    Update jenkins/

--- seed-jobs/b_cart.groovy
+++ seed-jobs/b_cart.groovy
@@ -1,8 +1,9 @@
 multibranchPipelineJob('CI-Pipelines/cart') {
   branchSources {
-    git {
-      id('123456789') // IMPORTANT: use a constant and unique identifier
-      remote('https://github.com/raghudevopsb63/cart.git')
+    github {
+      id('23232323') // IMPORTANT: use a constant and unique identifier
+      scanCredentialsId('GitHub')
+      repository('https://github.com/raghudevopsb63/cart')
     }
   }
   orphanedItemStrategy {
