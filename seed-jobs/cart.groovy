0aab57b5a284a35acac3f9cc99cf5eab929a4344
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-07 07:01:29 +0530

    Update jenkins/

--- seed-jobs/cart.groovy
+++ seed-jobs/cart.groovy
@@ -2,9 +2,7 @@ multibranchPipelineJob('example') {
   branchSources {
     git {
       id('123456789') // IMPORTANT: use a constant and unique identifier
-      remote('https://github.com/jenkinsci/job-dsl-plugin.git')
-      credentialsId('github-ci')
-      includes('JENKINS-*')
+      remote('https://github.com/raghudevopsb63/cart.git')
     }
   }
   orphanedItemStrategy {
